1d95263fe9c8f0a6ef7d421fa5c6c6eca426e7ce
helloilef <ilef.boualleeg@gmail.com>
2025-02-13 10:15:22 +0100

    trying to add all files

--- frontend/script.js
+++ frontend/script.js
@@ -1,10 +1,27 @@
-const loginBtn = document.getElementById('login');
-const closeBtn = document.querySelector('.close-btn');
-const loginForm = document.getElementById('login-form');
+const loginBtn = document.getElementById("login");
+const closeBtn = document.querySelector(".close-btn");
+const loginForm = document.getElementById("login-form");
+const cookBtn = document.getElementById("cook-btn");
+const dynamicCook = document.getElementById("dynamic-cook");
+const sectionContainer = document.querySelector(".section-container");
 
-loginBtn.addEventListener('click', () => { loginForm.style.display= 'block';
+loginBtn.addEventListener("click", () => {
+  loginForm.style.display = "block";
 });
 
-closeBtn.addEventListener('click',()=> {
-    loginForm.style.display = 'none';
-})
+closeBtn.addEventListener("click", () => {
+  loginForm.style.display = "none";
+});
+
+cookBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  fetch("cook.html")
+    .then((response) => response.text())
+    .then((data) => {
+      dynamicCook.innerHTML = data;
+      sectionContainer.style.display = "none";
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
